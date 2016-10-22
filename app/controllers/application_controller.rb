a3b0c9ad198e3ee5c1d23733c053997853bb5968
Juan Camilo <camilo9020@gmail.com>
2016-10-21 21:15:08 -0500

    Config annotate Config links to edit and signout sessions Style registrations/edit.htm.erb view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout :layout
+
+   private
+
+   def layout
+     if devise_controller? && devise_mapping.name == :admin
+       "admin"
+     else
+       "application"
+     end
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
+
   root 'pages#index'
   resources :subscribers
   get 'admin' => 'admin/dashboard#index'
+  devise_for :admins
 
   namespace :admin do
     resource :dashboard, only: [:index]
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
