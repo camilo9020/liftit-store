75bb4b2269d105f13fa86f79d705430d1a2af4d5
Juan Camilo <camilo9020@gmail.com>
2016-10-21 20:31:10 -0500

    Create dashboard controller Create admin layout Create dashboard/index view

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,9 @@
+class Admin::DashboardController < ApplicationController
+
+  layout 'admin'
+
+  def index
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#index'
   resources :subscribers
+  get 'admin' => 'admin/dashboard#index'
 
+  namespace :admin do
+    resource :dashboard, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
